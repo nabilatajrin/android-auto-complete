31d8a1cb1a070daa78aab9ab961b49fc4645ff0a
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-23 13:38:08 +0600

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/bd/org/rain/autocomplete/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/bd/org/rain/autocomplete/ExampleInstrumentedTest.java
@@ -3,12 +3,9 @@ package bd.org.rain.autocomplete;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -20,7 +17,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("bd.org.rain.autocomplete", appContext.getPackageName());
     }
 }
