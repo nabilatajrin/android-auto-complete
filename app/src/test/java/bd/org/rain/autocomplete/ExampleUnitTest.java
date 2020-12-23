498f97dbb65a653d3bb73e65c98ab61083b2ed11
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-23 13:37:48 +0600

    Update ExampleUnitTest.java

--- app/src/test/java/bd/org/rain/autocomplete/ExampleUnitTest.java
+++ app/src/test/java/bd/org/rain/autocomplete/ExampleUnitTest.java
@@ -1,9 +1,7 @@
 package bd.org.rain.autocomplete;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
